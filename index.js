2ba57c381ba7cb41bbca75a120355fbe6adebec4
omnia-adnan <o.m.a.1514.ja@gmail.com>
2024-02-04 12:22:55 +0300

    homwork done

---
+++ index.js
@@ -0,0 +1,20 @@
+var myName='omnia'
+console.log("your name",myName);
+var myAge=22
+console.log("your age",myAge);
+var student=true
+console.log("are your student",student);
+
+
+
+var myName='omnia'
+console.log("your name",typeof myName);
+var myAge=22
+console.log("your age",typeof myAge);
+var student=true
+console.log("are your student",typeof student);
+
+
+var greeting="Hello to our console"
+var name1="We are SCT"
+console.log(":)",greeting + " -",name1);
